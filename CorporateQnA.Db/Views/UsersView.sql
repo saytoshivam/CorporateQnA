72bf37438d659d4f69fb07bea7c69f699e08c8db
saytoshivam <shivamjaiswal171297@gmail.com>
2021-02-09 21:44:02 +0530

    Changed usersview

--- CorporateQnA.Db/Views/UsersView.sql
+++ CorporateQnA.Db/Views/UsersView.sql
@@ -1,7 +1,7 @@
-CREATE VIEW UsersView
+CREATE VIEW [UsersView]
 AS
-SELECT [AspNetUsers].[Id],[FullName],[ProfileImage],[JobLocation],[Designation],[Department], 
-COUNT([AskedBy]) AS [QuestionsAsked],
+SELECT [AspNetUsers].[Id],[FullName],[UserImage],[JobLocation],[Designation],[Department], 
+COUNT([AskedBy]) AS [QuestionsAsked],[LikedBy],[DisLikedBy],
 (
 SELECT COUNT([AnsweredBy])  
 FROM [AspNetUsers] [Users]  INNER JOIN [Answers] ON [Users].[Id]=[Answers].[AnsweredBy]
@@ -11,25 +11,8 @@ WHERE [Users].[Id]=[AspNetUsers].[Id]
 SELECT COUNT([IsBestSolution])  
 FROM [AspNetUsers] [Users]  INNER JOIN [Answers] ON [Users].[Id]=[Answers].[AnsweredBy]
 WHERE [Users].[Id]=[AspNetUsers].[Id] and [IsBestSolution]=1 
-) AS [QuestionsSolved],
-[LikedBy]='['+STUFF 
-(
-     (  
-       SELECT ',' + CAST(TRIM('[]' FROM [LikedBy]) AS VARCHAR(MAX))  
-       FROM [Answers] [a]   
-       WHERE [a].[AnsweredBy]=[AspNetUsers].[Id]
-       FOR XML PATH('')  
-     ),1,1,'' 
-)  +']',
-[DislikedBy]='['+STUFF 
-(
-     (  
-       SELECT ',' + CAST(TRIM('[]' FROM [DislikedBy]) AS VARCHAR(MAX))  
-       FROM [Answers] [a]   
-       WHERE [a].[AnsweredBy]=[AspNetUsers].[Id]
-       FOR XML PATH('')  
-     ),1,1,'' 
-)  +']'
+) AS [QuestionsSolved]
 FROM [AspNetUsers] LEFT JOIN
-[Questions] ON [Questions].[AskedBy]=[AspNetUsers].[Id]
-GROUP BY [AspNetUsers].[Id],[FullName],[ProfileImage],[JobLocation],[Designation],[Department];
+[Questions] ON [Questions].[AskedBy]=[AspNetUsers].[Id] LEFT JOIN [Answers]
+ON [Answers].[AnsweredBy]=[AspNetUsers].[Id] GROUP BY [AspNetUsers].[Id],[FullName],[UserImage],
+[JobLocation],[Designation],[Department],[LikedBy],[DislikedBy];
