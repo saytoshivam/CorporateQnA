e183be7e6b8ed831b6cb1e662d9467b19397be75
saytoshivam <shivamjaiswal171297@gmail.com>
2021-02-09 20:25:21 +0530

    Automapper added

---
+++ CorporateQnA.Services/Profiles/AutoMapperProfile.cs
@@ -0,0 +1,24 @@
+﻿using CorporateQnA.Models;
+using CorporateQnA.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace CorporateQnA.Services
+{
+    class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Data.Answer, Models.Answer>().ReverseMap();
+            CreateMap<Data.ApplicationUser, Models.ApplicationUser>().ReverseMap();
+            CreateMap<Data.Category, Models.Category>().ReverseMap();
+            CreateMap<Data.CategoryDetails, Models.CategoryDetails>().ReverseMap();
+            CreateMap<Data.Question, Models.Question>().ReverseMap();
+            CreateMap<Data.QuestionDetails, Models.QuestionDetails>().ReverseMap();
+            CreateMap<Data.Answer, Models.Answer>().ReverseMap();
+            CreateMap<Data.UserDetails, Models.UserDetails>().ReverseMap();
+        }
+    }
+}
