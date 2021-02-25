8da0a2272d16afaa080889cf7ebfcb8d765fc509
Shivam Jaiswal <shivam.j@technovert.net>
2021-02-25 16:56:37 +0530

    component and services structure added

--- CorporateQnA/CorporateQnA/src/app/app.module.ts
+++ CorporateQnA/CorporateQnA/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { AccountComponent } from './account/account.component';
+import { UserComponent } from './user/user.component';
+import { QuestionComponent } from './question/question.component';
+import { AnswerComponent } from './question/answer/answer.component';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
+import { EditorComponent } from './shared/editor/editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    AccountComponent,
+    UserComponent,
+    QuestionComponent,
+    AnswerComponent,
+    HomeComponent,
+    CategoryComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
