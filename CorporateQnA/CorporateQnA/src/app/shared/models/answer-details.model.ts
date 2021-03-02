d5d1b4e23ee1ebc549dbf4762755e87204cc2d50
saytoshivam <shivamjaiswal171297@gmail.com>
2021-03-02 20:07:28 +0530

    answermodel added

---
+++ CorporateQnA/CorporateQnA/src/app/shared/models/answer-details.model.ts
@@ -0,0 +1,17 @@
+export class AnswerDetails {
+    public id: number;
+
+    public fullName: string;
+
+    public answer: string;
+
+    public userImage: string;
+
+    public answeredOn: Date;
+
+    public totalLikes: number;
+
+    public totalDislikes: number;
+
+    public isBestSolution: boolean;
+}
